908370a7f00ae659e2aafdf14bb93c40f4d208bd
Neil Ranada <neil.ranada@gmail.com>
2022-02-12 02:20:19 -0800

    Able to start drag

--- client/src/components/DragNDrop.js
+++ client/src/components/DragNDrop.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, {useState, useRef} from 'react';
 
 function DragNDrop({data}) {
+
+    // Set list in a stata 'useState(data)' so we can update and make changes
+    const [list, setList] = useState(data);
+
+    // Stays constant between re-renders
+    const dragItem = useRef();
+
+    const handleDragStart = (e, params) => {
+        console.log('drag starting...', params);
+        dragItem.current = params;
+
+    }
+
     return (
         <div className="drag-n-drop">
-          {data.map((grp, grpI) => (
+          {list.map((grp, grpI) => (
             <div key={grp.title} className="dnd-group">
               <div className="group-title">{grp.title}</div> 
               {grp.items.map((item, itemI) => (
-                <div draggable key = {item} className="dnd-item">
+                <div draggable onDragStart={(e) => {handleDragStart(e,{grpI, itemI})}} key = {item} className="dnd-item">
                   {item}
                 </div>
               ))}
