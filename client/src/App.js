37d5957caff00c203ca5bc34fd43a4126f0796ec
hunterMotko <hunterMotko@gmail.com>
2022-02-19 13:53:17 -0500

    update structure for api data

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-
+import React, {useEffect, useState} from 'react'
 import './App.css';
 
 import DragNDrop from './components/DragNDrop';
@@ -8,14 +8,70 @@ const data = [
   { title: 'Doing', items: ['4', '5'] },
   { title: 'Done', items: ['6', '7'] }
 ]
+// TODO ideal structure make the frontend cards handle this data
+// const data2 = [
+//   {id:'', name:'', description:'', assignedTo:'', image:'', status: 'To Do',createdAt: ''},
+//   {id:'', name:'', description:'', assignedTo:'', image:'', status: 'To Do',createdAt: ''},
+//   {id:'', name:'', description:'', assignedTo:'', image:'', status: 'To Do',createdAt: ''},
+//   {id:'', name:'', description:'', assignedTo:'', image:'', status: 'To Do',createdAt: ''},
+//   {id:'', name:'', description:'', assignedTo:'', image:'', status: 'To Do',createdAt: ''},
+//   {id:'', name:'', description:'', assignedTo:'', image:'', status: 'To Do',createdAt: ''},
+//   {id:'', name:'', description:'', assignedTo:'', image:'', status: 'Doing',createdAt: ''},
+//   {id:'', name:'', description:'', assignedTo:'', image:'', status: 'Doing',createdAt: ''},
+//   {id:'', name:'', description:'', assignedTo:'', image:'', status: 'Doing',createdAt: ''},
+//   {id:'', name:'', description:'', assignedTo:'', image:'', status: 'Doing',createdAt: ''},
+//   {id:'', name:'', description:'', assignedTo:'', image:'', status: 'Doing',createdAt: ''},
+//   {id:'', name:'', description:'', assignedTo:'', image:'', status: 'Doing',createdAt: ''},
+//   {id:'', name:'', description:'', assignedTo:'', image:'', status: 'Done',createdAt: ''},
+//   {id:'', name:'', description:'', assignedTo:'', image:'', status: 'Done',createdAt: ''},
+//   {id:'', name:'', description:'', assignedTo:'', image:'', status: 'Done',createdAt: ''},
+//   {id:'', name:'', description:'', assignedTo:'', image:'', status: 'Done',createdAt: ''},
+//   {id:'', name:'', description:'', assignedTo:'', image:'', status: 'Done',createdAt: ''},
+//   {id:'', name:'', description:'', assignedTo:'', image:'', status: 'Done',createdAt: ''},
+// ]
 
 
-function App() {
+
+function App(props) {
+  // const [data, setData] = useState({})
+
+  // const filter = data => {
+  //   // filter to three columns
+  //   let res = data2.reduce((acc, cur)=>{
+  //     let key = cur.status
+  //     if(!acc[key]) {
+  //       acc[key] = []
+  //     }
+  //     acc[key].push(cur)
+
+  //     return acc
+  //   }, {})
+  // !set the data
+      // setData(res)
+  // }
+  // TODO OPTION LOAD ONE going to have to load data like this
+// useEffect(()=>{
+  //   fetch('some/api').then(res=>res.json()).then(res=>filter(res))
+  // }, [])
+
+  // TODO OPTION LOAD TWO or LIke this TO SHOW WHAT ASYNC/AWAIT LOOKS LIKE
+  // const loadData = async() =>{
+  //   let res = await fetch('/data')
+  //   let result = res.json()
+  //   filter(result)
+  // }
+  // useEffect(()=>{
+  //   loadData()
+  // }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <DragNDrop data={data} />
+        <h1>Qwasarello</h1>
       </header>
+      <section className='App-section'>
+        <DragNDrop data={data} />
+      </section>
     </div>
   );
 }
